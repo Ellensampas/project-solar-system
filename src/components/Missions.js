6a01447ae69a8a2bc64bdadfd925b46ab5dcc21a
ellen Sampaio <ellenssbarross@gmail.com>
2022-09-21 18:15:52 -0300

    adiciona requisito 10/finaliza projeto

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section className="mostra-planetas">
+          {missions.map((eleme) => (
+            <MissionCard
+              key={ eleme.name }
+              name={ eleme.name }
+              year={ eleme.year }
+              country={ eleme.country }
+              destination={ eleme.destination }
+            />
+          ))}
+        </section>
       </div>
     );
   }
